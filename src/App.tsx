8948d21c4bcb0258b9cfcec8cf86bbe301c2b13f
nndafnmain <nndafnmain@gmail.com>
2025-02-02 22:59:54 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-green-800 text-3xl">Jay</h1>
+      <Button>Hey, click</Button>
+    </main>
+  );
+}
+
+export default App;
